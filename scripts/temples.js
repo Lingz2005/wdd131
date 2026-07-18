aa5139d2d5d2df70b48f583dec95ea66376ee193
Lingz2005 <jessennadi64@gmail.com>
2026-07-18 13:08:48 -0700

    WDD

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,8 +1,6 @@
-// Footer dynamic year and last modified
 document.getElementById("year").textContent = new Date().getFullYear();
 document.getElementById("lastModified").textContent = document.lastModified;
 
-// Hamburger toggle
 const hamburger = document.getElementById("hamburger");
 const navMenu = document.getElementById("navMenu");
 
